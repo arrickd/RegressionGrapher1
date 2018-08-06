e735525565c94415fbd3651e9ea8e43a4d07057b
daniel <danarrick@gmail.com>
2018-08-06 15:51:43 -0400

    8/6

--- src/grapher/Regression.java
+++ src/grapher/Regression.java
@@ -3,13 +3,9 @@ package grapher;
 import graphs.Graph;
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.awt.Graphics;
 
 public class Regression extends Graph {
-    
-    //private Graphics draw;
-    
+       
     public Regression(String title, int width, int height){
         
        super(title,width, height);
@@ -17,20 +13,17 @@ public class Regression extends Graph {
         coordsX=new ArrayList<>();
         coordsY=new ArrayList<>();
         pairs=0;
-        enterData();
-      //  draw();        
+        enterData();       
     }
 
    // @Override
     public void enterData() {
 
-      //  Scanner scan = new Scanner(System.in);
-
-       // System.out.println("Number of coordinate pairs to enter?");
+        //EDIT ME TO REFLECT AMOUNT OF PAIRS TO GRAPH BELOW//
         pairs = 10;
-        
-        coordsX.add(20.0); coordsY.add(140.0);
-        coordsX.add(60.0); coordsY.add(180.0);
+        //EDIT ME/////////////////////////////
+        coordsX.add(20.0); coordsY.add(140.0); //pair 1
+        coordsX.add(60.0); coordsY.add(180.0); //pair 2..etc
         coordsX.add(130.0); coordsY.add(150.0);
         coordsX.add(95.0); coordsY.add(170.0);
         coordsX.add(75.0); coordsY.add(180.0);
@@ -39,44 +32,13 @@ public class Regression extends Graph {
         coordsX.add(185.0); coordsY.add(190.0);
         coordsX.add(100.0); coordsY.add(120.0);
         coordsX.add(200.0); coordsY.add(295.0);
-        
-        /*
-        int check = 0;
-        while (check < 5) {
-
-            check++;
-
-            System.out.println("enter x coord: ");
-            coordsX.add(scan.nextDouble());
-
-            System.out.println("enter y coord: ");
-            coordsY.add(scan.nextDouble());
-
-        }
-        */
-        
+        ///////////////////////////////////////
+   
         Operations.setMeanX(coordsX, pairs);
         Operations.setMeanY(coordsY, pairs);
         Operations.setCorrelation(coordsX, coordsY, pairs);
         Operations.setSlope(coordsX, coordsY, pairs);
-        Operations.setIntercept();
-        
-        /* WASTED TIME SORTING ARRAY
-        double temp =0;
-       
-        for (int i = 0; i<coordsX.size(); i++){
-            
-            for (int j =0; j<coordsX.size(); j++){
-            
-               if(coordsX.get(i)>coordsX.get(j)){
-                   
-                   temp =  coordsX.get(i);
-                   coordsX.set(i, coordsX.get(j));
-                   coordsX.set(j, temp);
-               }    
-            }  
-        }
-        */
+        Operations.setIntercept();        
     }
       
     public static void draw(){
@@ -87,7 +49,6 @@ public class Regression extends Graph {
             long y = Math.round(coordsY.get(i));
                                        
             g.setColor(Color.red);
-          //g.fillOval( (int)x-3, height-(int)y-3, 6, 6); 
             g.fillOval( (int)x,(int)y, 6, 6); 
         }
                 
